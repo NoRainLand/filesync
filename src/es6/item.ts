4d283064cd71a34a905fc8620efcd9b45808cd84
NoRain <wuxinrufeng@qq.com>
2024-01-05 14:57:04 +0800

    修改日期格式

--- src/es6/item.ts
+++ src/es6/item.ts
@@ -98,9 +98,19 @@ export class item {
         }
 
         // 显示时间戳
-        const date = new Date(data.timestamp);
-        const formattedDate = `${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-        this.txtDate.textContent = formattedDate;
+        this.txtDate.textContent = this.tranDate(data.timestamp);
+    }
+
+    tranDate(timestamp: number): string {
+        const date = new Date(timestamp);
+        const year = date.getFullYear();
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const day = ('0' + date.getDate()).slice(-2);
+        const hours = ('0' + date.getHours()).slice(-2);
+        const minutes = ('0' + date.getMinutes()).slice(-2);
+        const seconds = ('0' + date.getSeconds()).slice(-2);
+        const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        return formattedDate;
     }
 
     reSize(size: number): string {
